a8881ab75403dd43cf6d225f1cd3fa799dfbda7e
giovanni <you@gmail.com>
2024-09-28 15:57:22 +0300

    add footer

--- prepa/src/components/footer/Footer.tsx
+++ prepa/src/components/footer/Footer.tsx
@@ -4,19 +4,93 @@ const Footer = () => {
     return(
         <footer className="footer">
             <div className="footer__container">
+                
+                <div className="row">
+                    <div className="col-1">
+                        <h4 className="title">
+                            Tech Pharma
+                        </h4>
 
-                <div className="footer__social">
-                <a href="https://www.linkedin.com/in/giovanni-kevin-204422300/" className="home__social-icon" target='_blank'>
-                    <i className='bx bxs-phone'></i>
-                </a>
+                        <ul className="informations">
+                            <li>
+                                <a href="/fr/qui-sommes-nous" title="Qui sommes-nous ?">Qui sommes-nous ?</a>
+                            </li>
+                            <li>
+                                <a href="/fr/histoire-premiere-pharmacie-en-ligne" title="Notre histoire">Notre histoire</a>
+                            </li>
+                            <li>
+                                <a href="/fr/nos-services-en-pharmacie" title="Nos services">Nos services</a>
+                            </li>
+                            <li>
+                                <a href="/fr/parapharmacie" title="Parapharmacie">Parapharmacie</a>
+                            </li>   
+                            <li>
+                                <a className="recruitment" href="/fr/recrutement" title="Rejoignez-nous !">Rejoignez-nous !</a>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                    
+                    <div className="col-1">
+                        <h4>
+                            Informations
+                        </h4>
+                        <ul className="informations">
+                            <li className="item">
+                                <a href="/fr/aide/livraison" title="Livraison">Livraison</a>
+                            </li>
+                            <li className="item">
+                                <a href="/fr/faq" title="FAQ">FAQ</a>
+                            </li>
+                            <li className="item">
+                                <a href="/fr/aide/retour-remboursement" title="Réclamation &amp; Rétractation">Réclamation &amp; Rétractation</a>
+                            </li>
+                                    <li className="item">
+                                <a href="/fr/contact/" title="Contact">Contact</a>
+                            </li>
+                            <li className="item">
+                                <a href="/fr/rappels-lots" title="Rappels de lots">Rappels de lots</a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                    <div className="col-1">
+                        <h4>Informations légales</h4>
+                        <ul className="informations">
+                            <li>
+                                <a href="/fr/page/mentions-legales.html" title="Mentions légales">Mentions légales</a>
+                            </li>
+                            <li>
+                                <a href="/fr/page/protection-donnees-personnelles.html" title="Protection des données personnelles">Protection des données personnelles</a>
+                            </li>
+                            <li>
+                                <a href="/fr/conditions-generales-de-vente" title="Conditions Générales de Vente">Conditions Générales de Vente</a>
+                            </li>
+                            <li>
+                                <a href="/fr/conditions-des-offres" title="Conditions des offres">Conditions des offres</a>
+                            </li>
+                                    <li>
+                                <a href="javascript:window.cookieBar.display()">
+                                    Gérer mes cookies
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
 
-                <a href="https://www.instagram.com/giovanni_kevin_" className="home__social-icon" target='_blank'>
-                    <i className='bx bxl-messenger' ></i>
-                </a>
+                    <div className="col-1 social">
+                        <h4>Contact</h4>
+                        <a href="" className="home__social-icon" target='_blank'>
+                            <i className='bx bxs-phone'></i>
+                        </a>
 
-                <a href="https://github.com/Giovannikev/" className="home__social-icon" target='_blank'>
-                    <i className="bx bxl-instagram-alt"></i>
-                </a>
+                        <a href="" className="home__social-icon" target='_blank'>
+                            <i className='bx bxl-messenger' ></i>
+                        </a>
+
+                        <a href="" className="home__social-icon" target='_blank'>
+                            <i className="bx bxl-instagram-alt"></i>
+                        </a>
+                    </div>
                 </div>
 
                 <span className="footer__copy">
