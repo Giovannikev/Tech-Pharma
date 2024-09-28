23f056823f7df71f9ec3bf31f50ba7ed550c49b5
giovanni <you@gmail.com>
2024-09-28 16:06:33 +0300

    modif

--- prepa/src/components/home/Home.tsx
+++ prepa/src/components/home/Home.tsx
@@ -2,10 +2,10 @@ import './home.css'
 
 const Home = () => {
     return(
-        <section className='home section' id='home'>
-            <div className="home__container container grid">
+        <section className='home' id='home'>
+            <div className="home__container">
                 <div className="content">
-                    <h1 className="title">Bienvenue sur votre pharmacie en ligne </h1>
+                    <h1 className="title">Bienvenue sur votre Pharmacie en ligne </h1>
                     <div className="home-image">
                         <img src="https://cdn.pixabay.com/photo/2023/09/20/07/37/doctor-8264060_640.jpg" alt="Pharmacie en ligne - Tech Pharma"/>
                     </div>
