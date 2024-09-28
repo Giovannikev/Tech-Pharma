5c9f42e9ab656b29bc2ddfe14903e5643c37ab27
giovanni <you@gmail.com>
2024-09-28 16:48:42 +0300

    add button

--- prepa/src/components/home/Home.tsx
+++ prepa/src/components/home/Home.tsx
@@ -5,10 +5,16 @@ const Home = () => {
         <section className='home' id='home'>
             <div className="home__container">
                 <div className="content">
-                    <div className="title">Bienvenue sur notre site Pharmacie en ligne </div>
+
+                    <div className="content-left">
+                        <h1>Bienvenue sur notre site Pharmacie en ligne</h1> 
+                        <button type="button" className='home__button'>Créer un Compte</button>
+                    </div>
+
                     <div className="home-image">
                         <img src="https://cdn.pixabay.com/photo/2013/07/13/11/57/apothecary-159037_640.png" alt="Pharmacie en ligne - Tech Pharma"/>
                     </div>
+
                 </div>
             </div>
         </section>
