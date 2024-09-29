4c07f9c0e2d55111633ea0f49c49cff43f606410
giovanni <you@gmail.com>
2024-09-29 13:02:44 +0300

    modification

--- prepa/src/components/Products/ProductsData.tsx
+++ prepa/src/components/Products/ProductsData.tsx
@@ -8,21 +8,21 @@ const productsData = [
     name: 'Paracétamol 500mg',
     description: 'Un analgésique couramment utilisé pour traiter la douleur légère à modérée et la fièvre.',
     price: '5.000 Ar',
-    image: '',
+    image: 'https://media.istockphoto.com/id/1217213618/fr/photo/comprim%C3%A9s-g%C3%A9n%C3%A9riques-de-parac%C3%A9tamol-500mg.jpg?s=612x612&w=0&k=20&c=0GXmAUlK526Ia1EBtD5yll4JZIySn-kVxpj5GJxyXD8=',
   },
   {
     id: 2,
     name: 'Ibuprofène 200mg',
     description: 'Anti-inflammatoire non stéroïdien utilisé pour réduire la douleur et l’inflammation.',
     price: '10.000 Ar',
-    image: '',
+    image: 'https://media.istockphoto.com/id/1022216112/fr/photo/paquet-dibuprof%C3%A8ne-g%C3%A9n%C3%A9rique-comprim%C3%A9s.jpg?s=612x612&w=0&k=20&c=Hh8caNPmrFVg3XMKnzgyD0DeP3QDs1vl3cBxf2LwX4s=',
   },
   {
     id: 3,
     name: 'Vitamine C 1000mg',
     description: 'Supplément pour renforcer le système immunitaire et prévenir les carences en vitamine C.',
     price: '12.000 Ar',
-    image: '',
+    image: 'https://media.istockphoto.com/id/459004449/fr/photo/haliborange-vitamine-c-1-mg-effervescentes-tablettes.jpg?s=612x612&w=0&k=20&c=Uh2ifbZSik84UfPqU-OG9Iv55dd4ccwto47auQL0g68=',
   },
   // Ajoute autant de produits que nécessaire
 ];
